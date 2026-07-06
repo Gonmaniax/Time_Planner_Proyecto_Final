9c4d495779f77327b5baaa9c32463e27fc125cec
Gonther <henry_forero@outlook.com>
2026-07-06 06:00:50 -0500

    Update email configuration for TLS and SSL
    Comment out MAIL_USE_TLS and enable MAIL_USE_SSL.

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -43,7 +43,8 @@ app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
 app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
-app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = True
 
 mail.init_app(app)
 app.register_blueprint(recuperacion_bp, url_prefix="/api/recuperacion")
